74f962009bd42578fc237c54f76e66e10681b046
Mert Turan <linuxturkey06@gmail.com>
2023-12-09 01:47:04 +0300

    new-branch

--- functions.py
+++ functions.py
@@ -1,15 +1,42 @@
 import os
-import pandas as pd
+import pandas as pd, re
 from datetime import datetime
 
+def sort_folders(folders):
+    def natural_keys(text):
+        def atoi(text):
+            return int(text) if text.isdigit() else text
+
+        return [atoi(c) for c in re.split(r'(\d+)', text)]
+
+    return sorted(folders, key=natural_keys)
+
+def validate_layer(string):
+    pattern = r"^Layer\s\d+$"
+    return re.match(pattern, string) is not None
 
 def list_folders(directory: str):
     #directory = directory.replace("file://","")
     folders = [folder for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder))]
-    return folders
+    sorted_folders = sort_folders(folders)  # Klasörleri sırala
+
+    return sorted_folders
 
 def list_files(directory: str):
     #directory = directory.replace("file://","")
      #if f.endswith(".xlsx")
     folders = [f for f in os.listdir(directory)]
     return folders
+
+def get_layers(file_path):
+    try:
+        sheets = []
+        excel_file = pd.ExcelFile(file_path)
+        sheet_names = excel_file.sheet_names
+        for name in sheet_names:
+             if validate_layer(name):
+                 sheets.append(name)
+        return sheets
+    except Exception as e:
+        print(f"Excel Layer Tespit Edilirken Hata oluştu: {e}")
+        return []
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys, os
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QListWidget, QListWidget, QRadioButton, QFileDialog
 from PyQt6.QtCore import QFile
-from functions import list_folders
+from PyQt6.QtGui import QIcon
+from functions import list_folders, list_files, get_layers
 from PyQt6.uic import loadUi
 
 class MyWindow(QMainWindow):
@@ -17,34 +18,79 @@ class MyWindow(QMainWindow):
         # UI dosyasını yükleme
         loadUi("main.ui", self)
 
+        self.setWindowIcon(QIcon("icon.png")) 
+        self.setWindowTitle("DeepSoil Analiz")
+
     def initUiElements(self):
         self.analiz = self.findChild(QListWidget, "listWidget")
         self.deprem = self.findChild(QListWidget, "listWidget_2")
         self.layer = self.findChild(QListWidget, "listWidget_3")
         self.browse = self.findChild(QPushButton, "pushButton")
         self.run = self.findChild(QPushButton, "pushButton_2")
-        self.radio_analiz = self.findChild(QRadioButton, "radioButton")
-        self.radio_deprem = self.findChild(QRadioButton, "radioButton_2")
+        self.radio_analiz = self.findChild(QRadioButton, "radioButton_2")
+        self.radio_deprem = self.findChild(QRadioButton, "radioButton")
+        self.radio_layer = self.findChild(QRadioButton, "radioButton_3")
+
 
     def interactions(self):
             self.browse.clicked.connect(self.browsedir)
-      
-
-    def check_radio_buttons(self):
+            self.analiz.currentItemChanged.connect(self.init_analiz)
+            self.deprem.currentItemChanged.connect(self.init_layer)
+            self.radio_analiz.clicked.connect(self.trigger_radio)
+            self.radio_deprem.clicked.connect(self.trigger_radio)
+            self.radio_layer.clicked.connect(self.trigger_radio)
+                
+    def trigger_radio(self):
         if self.radio_analiz.isChecked():
-            print("Analiz radyo düğmesi seçildi.")
+            print("ANALiz")
+            self.deprem.clear()
+            self.layer.clear()
+            self.analiz.setSelectionMode(QListWidget.SelectionMode.MultiSelection)
+            self.deprem.setSelectionMode(QListWidget.SelectionMode.SingleSelection)
+            self.layer.setSelectionMode(QListWidget.SelectionMode.SingleSelection)  
         elif self.radio_deprem.isChecked():
-            print("Deprem radyo düğmesi seçildi.")
+            print("DEPREM")
+            self.deprem.clear()
+            self.layer.clear()
+            self.analiz.setSelectionMode(QListWidget.SelectionMode.SingleSelection)
+            self.deprem.setSelectionMode(QListWidget.SelectionMode.MultiSelection)
+            self.layer.setSelectionMode(QListWidget.SelectionMode.SingleSelection)  
+        elif self.radio_layer.isChecked():
+            print ("LAYER")
+            self.deprem.clear()
+            self.layer.clear()
+            self.analiz.setSelectionMode(QListWidget.SelectionMode.SingleSelection)
+            self.deprem.setSelectionMode(QListWidget.SelectionMode.SingleSelection)
+            self.layer.setSelectionMode(QListWidget.SelectionMode.MultiSelection)  
         else:
-            print("Hiçbir radyo düğmesi seçilmedi.")
+            print("...")    
 
 
     def browsedir(self):
+        self.analiz.clear()
         HOME_PATH = os.path.expanduser('~\Documents\DEEPSOIL 7')
         self.browse = QFileDialog.getExistingDirectory(self,directory=HOME_PATH)
-        print (self.browse)
         folders = list_folders(self.browse)
         self.analiz.addItems(folders)
+
+    
+    def init_analiz(self):
+        self.deprem.clear()
+        text = self.analiz.currentItem()
+        filespath = f"{self.browse}/{text.text()}"
+        files = list_files(filespath)
+        self.deprem.addItems(files)
+
+    def init_layer(self):
+        self.layer.clear()
+        analiz = self.analiz.currentItem()
+        deprem = self.deprem.currentItem()
+        if analiz is not None and deprem is not None:
+            path = f"{self.browse}/{analiz.text()}/{deprem.text()}"
+            layers = get_layers(path)
+            self.layer.addItems(layers)
+        
+        
         
 def main():
     app = QApplication(sys.argv)
